d245f9864f6192395d54296169d76a0d2a1a8fbe
Austin DeGarmo <austindegarmo@yahoo.com>
2018-01-30 16:24:48 -0800

    first push

---
+++ minesweeper/src/minesweeper.js
@@ -0,0 +1,15 @@
+console.log("This is how an empty board will show up")
+
+console.log('  |   |  ')
+console.log('  |   |  ')
+console.log('  |   |  ')
+
+
+console.log("This is what a board with a guess and a bomb on it would look like:")
+
+const guessLine = '1 |   |  '
+const bombLine = '  | B |  '
+console.log(guessLine)
+console.log(bombLine)
+console.log('  |   |  ')
+
