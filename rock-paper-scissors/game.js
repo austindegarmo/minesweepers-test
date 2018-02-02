819fa8932e541c0a06b8992cc9e6dcf45a89a14a
Austin DeGarmo <austindegarmo@yahoo.com>
2018-02-01 17:26:04 -0800

    onemillionthpushsweepstakes

--- rock-paper-scissors/game.js
+++ rock-paper-scissors/game.js
@@ -4,6 +4,26 @@ const paper = "p";
 
 const scissors = "s";
 
+let userInput = userInput.toLowerCase();
+
 const getUserChoice() => (userInput) {
 
-};
+	if (userInput = rock,paper,scissors) {
+
+		return userInput
+
+		else throw error 
+	}
+
+}
+const getComputerChoice() => {
+
+	choice = Math.random() * 2;
+
+	pcchoice = choice.Math.floor();
+
+	if (pcchoice =  1) {
+			pcchoice  = 
+	}
+}
+
