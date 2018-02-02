52ea045999e863b40e28c482347ef2e6ed188211
Austin DeGarmo <austindegarmo@yahoo.com>
2018-02-01 16:24:36 -0800

    Second Push

--- minesweeper/src/minesweeper.js
+++ minesweeper/src/minesweeper.js
@@ -1,15 +1,28 @@
-console.log("This is how an empty board will show up")
+// console.log("This is how an empty board will show up")
 
-console.log('  |   |  ')
-console.log('  |   |  ')
-console.log('  |   |  ')
+// console.log('  |   |  ')
+// console.log('  |   |  ')
+// console.log('  |   |  ')
 
 
-console.log("This is what a board with a guess and a bomb on it would look like:")
+// console.log("This is what a board with a guess and a bomb on it would look like:")
 
-const guessLine = '1 |   |  '
-const bombLine = '  | B |  '
-console.log(guessLine)
-console.log(bombLine)
-console.log('  |   |  ')
+// const guessLine = '1 |   |  '
+// const bombLine = '  | B |  '
+// console.log(guessLine)
+// console.log(bombLine)
+// console.log('  |   |  ')
 
+//New code that isn't hard coded to fuckall
+
+// const grid = console.log('  |   |  ')
+
+ function onstart(grid) {
+ 	console.log('  |   |  ')
+	console.log('  |   |  ')
+	console.log('  |   |  ')
+
+
+ }
+
+ onstart();
