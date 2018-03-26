437f01244b5b7280b2597ef778ee3d4c6e73076a
Austin DeGarmo <austindegarmo@yahoo.com>
2018-03-26 12:42:14 -0700

    ravenous part two first push

--- ravenous/ravenous/src/App.js
+++ ravenous/ravenous/src/App.js
@@ -2,6 +2,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class business extends React.Component {
+  render() {
+<div className="Business">
+  <div className="image-container">
+    <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
+  </div>
+  <h2>MarginOtto Pizzeria</h2>
+  <div className="Business-information">
+    <div className="Business-address">
+      <p>1010 Paddington Way</p>
+      <p>Flavortown</p>
+      <p>NY 10101</p>
+    </div>
+    <div className="Business-reviews">
+      <h3>ITALIAN</h3>
+      <h3 className="rating">4.5 stars}</h3>
+      <p>90 reviews</p>
+    </div>
+  </div>
+</div>
+
+  }
+  const businesses = [
+    business,
+    business,
+    business,
+    business,
+    business,
+    business
+  ]
+
 class App extends Component {
   render() {
     return (
