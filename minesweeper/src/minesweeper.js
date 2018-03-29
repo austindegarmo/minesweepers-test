ff94910d80ac4053a943d67644b9f5459673e283
Austin DeGarmo <austindegarmo@yahoo.com>
2018-03-29 11:46:36 -0700

    fourth third push

--- minesweeper/src/minesweeper.js
+++ minesweeper/src/minesweeper.js
@@ -42,8 +42,17 @@ const getNumberOfNeighborBombs = (bombBoard, rowIndex, columnIndex) => {
   const numberOfRows = bombBoard.length;
   const numberOfColumns = [bombBoard[0].length;
   let numberOfBombs = 0;
-  neighborOffsets.forEach([0]);
-  
+  neighborOffsets.forEach(offset => {
+    const neighborRowIndex = rowIndex + offset[0];
+    const neighborColumnIndex = columnIndex + offset[1];
+    if (neighborRowIndex >= 0 && neighborRowIndex < numberOfRows && neighborColumnIndex >= 0 && neighborColumnIndex < numberOfColumns) {
+      if(bombBoard[neighborRowIndex]['B'] == false) {
+        
+      }
+
+    }
+  });
+
 
 
 }
